3de17ba56cf7f3371976692d46556cc7cb42ee73
Emiliyan Pavlov <emiliyan.pavlov@insait.ai>
2026-03-25 03:08:29 +0200

    temp: add base path for github.io preview

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  base: '/BgGPT',
 
   integrations: [
     tailwind({
